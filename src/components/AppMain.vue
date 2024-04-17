2e8b67168434bcbac482f7bd1c422a0b280fdf82
Marilisa Moio <moiomarilisa@gmail.com>
2024-04-17 16:34:52 +0200

    feat: add milestone 1

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,34 @@
 <script>
+    import { store } from "../store.js"
     export default{
-        name: 'AppMain'
+        name: 'AppMain',
+        data(){
+            return {
+                store
+            }
+        }
     }
 </script>
 
 <template>
-    this is a placeholder
+    <div class="container">
+        <div class="movie-card" v-for="movie in store.theseMovies">
+            <div class="name">Name: {{ movie.title }}</div>
+            <div class="original-name">Original name: {{ movie.original_title }}</div>
+            <div class="lang">Original language: {{ movie.original_language }}</div>
+            <div class="rating">Rating: {{ movie.vote_average }}</div>
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss">
+    .container{
+        display: flex;
+        flex-wrap: wrap;
+        gap: 20px;
+        .movie-card{
+            width: calc(100% / 4 - 15px);
+        }
+    }
 
 </style>
