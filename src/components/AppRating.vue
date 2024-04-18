2661bd0b534e42d5c91ce34a1ce41a5010474426
Marilisa Moio <moiomarilisa@gmail.com>
2024-04-18 14:16:26 +0200

    feat: add milestone 3

---
+++ src/components/AppRating.vue
@@ -0,0 +1,49 @@
+<script>
+    export default{
+        name: 'AppRating',
+        props: ["mediaRating"],
+        data(){
+            return {
+                stars: "",
+                numberOfStars: 5,
+                remainingStars: 0
+            }
+        },
+        methods: {
+            getTotalStars(rating){
+                const halfRating = rating / 2;;
+                const numberOfStar = Math.ceil(halfRating.toFixed(1))
+                const remainingStars = 5 - numberOfStar;
+                for(let i = 1; i <= remainingStars; i++){
+                    --this.numberOfStars
+                    ++this.remainingStars
+                }
+                console.log(this.numberOfStars, this.remainingStars)
+                
+            }
+        },
+        mounted(){
+            this.getTotalStars(this.mediaRating)
+        }
+    }
+</script>
+
+<template>
+    <div class="rating">
+        <i v-for="n in this.numberOfStars" class="fa-solid fa-star yellow"></i>
+        <i v-for="n in this.remainingStars" class="fa-solid fa-star gray"></i>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .yellow{
+        color:darkorange;
+    }
+    .gray{
+        color:lightgray
+    }
+    i{
+        text-shadow: 0px 0px 2px black;
+    }
+
+</style>
--- src/components/MediaCard.vue
+++ src/components/MediaCard.vue
@@ -1,26 +1,33 @@
 <script>
     import AppLanguage from "./AppLanguage.vue";
+    import AppRating from "./AppRating.vue"
 
     export default{
         name: 'MediaCard',
         components: {
-            AppLanguage
+            AppLanguage,
+            AppRating
         },
-        props: ["media"]
+        props: ["media"],
     }
 </script>
 
 <template>
     <div class="media-card">
+        <!-- TODO: controllare il bad gateway (no problemi alle immagini stampate) -->
+        <img :src="`https://image.tmdb.org/t/p/w342${media.poster_path}`" alt="">
         <div class="name">Name: {{ media.title || media.name }}</div>
         <div class="original-name">Original name: {{ media.original_title || media.original_name }}</div>
         <AppLanguage :mediaLang="media.original_language"></AppLanguage> 
-        <div class="rating">Rating: {{ media.vote_average }}</div>
+        <AppRating :mediaRating="media.vote_average"></AppRating>
     </div>
 </template>
 
 <style scoped lang="scss">
     .media-card{
             width: calc(100% / 4 - 15px);
+            img {
+                width: 100%;
+            }
         }
 </style>
